45a818881df455e01f5307f6812657c7dd443402
jonasbn <jonasbn@gmail.com>
2020-06-05 08:50:25 +0200

    Removed exit codes from error messages

--- entrypoint.sh
+++ entrypoint.sh
@@ -33,8 +33,8 @@ pyspelling --config $SPELLCHECK_CONFIG_FILE
 
 EXITCODE=$?
 
-test $EXITCODE -gt 1 && echo "($EXITCODE) Spelling check action failed, please check diagnostics";
+test $EXITCODE -gt 1 && echo "Spelling check action failed, please check diagnostics";
 
-test $EXITCODE -eq 1 && echo "($EXITCODE) Files in repository contain spelling errors";
+test $EXITCODE -eq 1 && echo "Files in repository contain spelling errors";
 
 exit $EXITCODE
