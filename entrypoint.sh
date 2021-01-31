bf4a547070387f32fd7a321c12df4c524fe56f35
Matt Calvert <miff2000@users.noreply.github.com>
2021-01-31 20:05:36 +0000

    Add source_files and task_name options (#34)
    * Add source_files and task_name options
    * `source_files` allows a list of specific files to scan be provided
    * `task_name` allows a specific task to be executed
    
    * Add example usage of new options
    
    Co-authored-by: Jonas Brømsø <jonasbn@users.noreply.github.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,11 +16,25 @@ else
     fi
 fi
 
+if [ -n "$INPUT_SOURCE_FILES" ]; then
+    if [ -z "$INPUT_TASK_NAME" ]; then
+        echo "task_name must be specified to use source_files option"
+        exit 1
+    fi
+    for FILE in $INPUT_SOURCE_FILES; do
+        SOURCES_LIST="$SOURCES_LIST --source $FILE"
+    done  
+fi
+
+if [ -n "$INPUT_TASK_NAME" ]; then
+    TASK_NAME="--name $INPUT_TASK_NAME"
+fi
+
 echo ""
 echo "Using pyspelling on repository files outlined in >$SPELLCHECK_CONFIG_FILE<"
 echo "----------------------------------------------------------------"
 
-pyspelling --config $SPELLCHECK_CONFIG_FILE
+pyspelling --config $SPELLCHECK_CONFIG_FILE $TASK_NAME $SOURCES_LIST
 
 EXITCODE=$?
 
