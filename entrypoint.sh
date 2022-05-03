bac226abd5228cde2172705af88a213e90c55be6
jonasbn <jonasbn@gmail.com>
2022-05-03 22:14:16 +0200

    This is first shot at a fix of the issue reported in #86. I believe file paths without space has been working as a conincidence since the splitting has not been working at all, but treating the list of file names as a single string also worked.
    Now it should work for both file paths without spaces
    and file paths with spaces, which however require
    uniform quoting using either ' (single quotes) or
    " (double quotes)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -l
+#!/bin/bash
 
 SPELLCHECK_CONFIG_FILE=''
 
@@ -19,17 +19,65 @@ fi
 echo ""
 echo "Using pyspelling on configuration outlined in >$SPELLCHECK_CONFIG_FILE<"
 
+SINGLE="'"
+DOUBLE='"'
+SPLIT=false
+
 if [ -n "$INPUT_SOURCE_FILES" ]; then
+
+    if grep -q $SINGLE <<< "$INPUT_SOURCE_FILES"; then
+        OIFS=$IFS
+        IFS=$SINGLE
+        SPLIT=true
+        echo "Detected separator: >$SINGLE< (single quote)"
+    fi
+
+    if grep -q $DOUBLE <<< "$INPUT_SOURCE_FILES"; then
+        OIFS=$IFS
+        IFS=$DOUBLE
+        SPLIT=true
+        echo "Detected separator: >$DOUBLE< (double quote)"
+    fi
+
     if [ -z "$INPUT_TASK_NAME" ]; then
         echo "task_name must be specified to use source_files option"
         exit 1
     else
         echo "Running task >$INPUT_TASK_NAME<"
     fi
-    for FILE in $INPUT_SOURCE_FILES; do
-        SOURCES_LIST="$SOURCES_LIST --source $FILE"
-        echo "Checking file >$FILE<"
-    done
+
+    if [ "$SPLIT" = true ]; then
+        echo "IFS = >$IFS<"
+        read -a arr <<< "$INPUT_SOURCE_FILES"
+
+        for FILE in "${arr[@]}"; do
+
+            # Skip null items
+            if [ -z "$FILE" ]; then
+                continue
+            fi
+
+            if [ "$FILE" = ' ' ]; then
+                continue
+            fi
+
+            SOURCES_LIST="$SOURCES_LIST --source $FILE"
+            echo "Checking quoted file >$FILE<"
+        
+        done
+
+        IFS=$OIFS
+        unset OIFS
+
+    else 
+        read -a arr <<< "$INPUT_SOURCE_FILES"
+
+        for FILE in "${arr[@]}"; do
+            SOURCES_LIST="$SOURCES_LIST --source $FILE"
+            echo "Checking file >$FILE<"
+        done
+    fi
+
 else
     echo "Checking files matching specified outlined in >$SPELLCHECK_CONFIG_FILE<"
 fi
