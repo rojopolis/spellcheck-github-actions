3aaf2627e00aae00c86d13ee4067a706ec69b274
jonasbn <jonasbn@gmail.com>
2024-10-13 18:45:07 +0200

    Cleaned and added documentation

--- pwc.py
+++ pwc.py
@@ -1,5 +1,9 @@
 #!python3
 
+# This little helper script was implemented to extract the sources from the spellcheck configuration file
+# The name comes from Python WCMatch, which is used to match the files against the sources
+# That is the short name I call it PriceWaterhouseCoopers, since it revises the file listing
+
 # read file and interpret it as yaml
 def read_yaml(file):
 
@@ -9,7 +13,6 @@ def read_yaml(file):
 
 import sys
 import yaml
-#import pprint
 from wcmatch import glob
 
 # read filename from command line as first argument
@@ -20,22 +23,14 @@ data = read_yaml(spellcheck_configuration_file)
 # fetch the sources from the YAML data
 sources = data.get('matrix')[0].get('sources')
 
-# print the sources from the YAML data
-#pprint.pprint(sources)
-
 for changed_file in sys.stdin:
 	if 'q' == changed_file.rstrip():
 		break
 	changed_file = changed_file.rstrip()
-	#print(f'Input : {changed_file}')
 
 	matched = glob.globmatch(changed_file, sources, flags=glob.NEGATE | glob.GLOBSTAR | glob.SPLIT)
 
 	if matched:
-		#print("Matched file:", changed_file)
 		exit(0)
 	else:
-		#print("No match for file:", changed_file)
 		exit(1)
-
-#pprint.pprint(files)
