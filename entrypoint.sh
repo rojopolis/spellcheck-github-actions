296bdcdf6160aa970ffc4e30aa656b0b404c8eb0
Andreas Kohout <andreas.kohout@annalise.ai>
2026-02-27 10:19:03 +0100

    Support spaces in file names (#322)
    * Support spaces in file names
    
    * Quote task variable

--- entrypoint.sh
+++ entrypoint.sh
@@ -32,6 +32,7 @@ echo "----------------------------------------------------------------"
 SINGLE="'"
 DOUBLE='"'
 SPLIT=false
+SOURCES_LIST=()
 
 if [ -n "$INPUT_SOURCE_FILES" ]; then
 
@@ -82,7 +83,7 @@ if [ -n "$INPUT_SOURCE_FILES" ]; then
                 continue
             fi
 
-            SOURCES_LIST="$SOURCES_LIST --source $FILE"
+            SOURCES_LIST+=("--source" "$FILE")
             echo "Checking quoted file >$FILE<"
 
         done
@@ -102,12 +103,12 @@ if [ -n "$INPUT_SOURCE_FILES" ]; then
                 echo "Skipping file >$FILE<"
                 continue
             fi
-            SOURCES_LIST="$SOURCES_LIST --source $FILE"
+            SOURCES_LIST+=("--source" "$FILE")
             echo "Checking file >$FILE<"
         done
     fi
 
-    echo "Checking files specification in sources_list as: >$SOURCES_LIST<"
+    echo "Checking files specification in sources_list as: >${SOURCES_LIST[*]}<"
 
 else
     echo "Checking files matching specification outlined in: >$SPELLCHECK_CONFIG_FILE<"
@@ -148,22 +149,21 @@ fi
 
 EXITCODE=0
 
-# shellcheck disable=SC2086
 # Command line template
-# pyspelling --verbose --config "$SPELLCHECK_CONFIG_FILE" --spellchecker "$SPELL_CHECKER" --name $TASK_NAME --source $SOURCES_LIST
+# pyspelling --verbose --config "$SPELLCHECK_CONFIG_FILE" --spellchecker "$SPELL_CHECKER" --name "$TASK_NAME" --source "FILE 1" --source "FILE 2" --source "FILE N"
 # source and name are included in the parameters used
 
-if [ -n "$INPUT_OUTPUT_FILE" ] && [ -n "$SOURCES_LIST" ]; then
-    $COMMAND --config "$SPELLCHECK_CONFIG_FILE" --spellchecker "$SPELL_CHECKER" $TASK_NAME $SOURCES_LIST | tee "$INPUT_OUTPUT_FILE"
+if [ -n "$INPUT_OUTPUT_FILE" ] && [ "${#SOURCES_LIST[@]}" -gt 0 ]; then
+    $COMMAND --config "$SPELLCHECK_CONFIG_FILE" --spellchecker "$SPELL_CHECKER" "$TASK_NAME" "${SOURCES_LIST[@]}" | tee "$INPUT_OUTPUT_FILE"
     EXITCODE=${PIPESTATUS[0]}
 elif [ -n "$INPUT_OUTPUT_FILE" ]; then
-    $COMMAND --config "$SPELLCHECK_CONFIG_FILE" --spellchecker "$SPELL_CHECKER" $TASK_NAME | tee "$INPUT_OUTPUT_FILE"
+    $COMMAND --config "$SPELLCHECK_CONFIG_FILE" --spellchecker "$SPELL_CHECKER" "$TASK_NAME" | tee "$INPUT_OUTPUT_FILE"
     EXITCODE=${PIPESTATUS[0]}
-elif [ -n "$SOURCES_LIST" ]; then
-    $COMMAND --config "$SPELLCHECK_CONFIG_FILE" --spellchecker "$SPELL_CHECKER" $TASK_NAME $SOURCES_LIST
+elif [ "${#SOURCES_LIST[@]}" -gt 0 ]; then
+    $COMMAND --config "$SPELLCHECK_CONFIG_FILE" --spellchecker "$SPELL_CHECKER" "$TASK_NAME" "${SOURCES_LIST[@]}"
     EXITCODE=$?
 elif [ -z "$INPUT_SOURCE_FILES" ]; then
-    $COMMAND --config "$SPELLCHECK_CONFIG_FILE" --spellchecker "$SPELL_CHECKER" $TASK_NAME
+    $COMMAND --config "$SPELLCHECK_CONFIG_FILE" --spellchecker "$SPELL_CHECKER" "$TASK_NAME"
     EXITCODE=$?
 else
     echo "No files to check, exiting"
